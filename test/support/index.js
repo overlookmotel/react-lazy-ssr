c0a05824fe5baa6edf413af1e871b4a4828b2421
overlookmotel <theoverlookmotel@gmail.com>
2020-04-06 23:56:50 +0100

    Tests: Simplify promise rejection handling

--- test/support/index.js
+++ test/support/index.js
@@ -6,12 +6,8 @@
 'use strict';
 
 /*
- * Throw any unhandled promise rejections.
- * For easier debugging of where an unhandled rejection is coming from,
- * if rejected Promise has a `._name` property, it is included in the error message.
+ * Throw any unhandled promise rejections
  */
-process.on('unhandledRejection', (err, promise) => {
-	let ctr = promise.constructor.name;
-	if (promise._name) ctr += ` ${promise._name}`;
-	throw new Error(`Unhandled rejection: ${(err || {}).message} (${ctr})`);
+process.on('unhandledRejection', (err) => {
+	throw err;
 });
